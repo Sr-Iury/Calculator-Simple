fa3239f4995fa9d58eacc7738146dfe273630700
Sr-Iury <iury.silvafim@gmail.com>
2024-10-11 11:08:57 -0300

    feat: adding the concatenation of numbers in the paragraph :sparkles:

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //A Função abaixo ela reivindica um parametro que vai ser os dados a serem clicados, e por meio deste, obtemos
 //comandos de inserção de dados que é feita pela innerhtml através da reivindicaçaõ do result
 function insertData(number){
-    document.getElementById('result').innerHTML = number;
+    var SavedNumber = document.getElementById('result').innerHTML;
+    SavedNumber = document.getElementById('result').innerHTML = SavedNumber + number;
 }
