5c97b0ccb124bd89f39a9d90da074b2d8469efdc
Sr-Iury <iury.silvafim@gmail.com>
2024-10-11 11:01:44 -0300

    feat: adding insert function to paragraph field :sparkles:

---
+++ script.js
@@ -0,0 +1,5 @@
+//A Função abaixo ela reivindica um parametro que vai ser os dados a serem clicados, e por meio deste, obtemos
+//comandos de inserção de dados que é feita pela innerhtml através da reivindicaçaõ do result
+function insertData(number){
+    document.getElementById('result').innerHTML = number;
+}
